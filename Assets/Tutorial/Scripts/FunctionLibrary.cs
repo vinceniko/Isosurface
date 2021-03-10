42c62463455f337a2b6566ece88e40d1b935954b
Vincent Nikolayev <vincent.nikolayev@gmail.com>
2021-03-09 19:39:15 -0500

    init

---
+++ Assets/Tutorial/Scripts/FunctionLibrary.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using static UnityEngine.Mathf;
+
+public static class FunctionLibrary
+{
+
+    public delegate Vector3 Function(float u, float v, float t);
+
+    public enum FunctionName { Wave, MultiWave, Ripple, Sphere, Torus }
+
+    static int functionLength = 5;
+
+    public static FunctionName GetNextFunctionName(FunctionName name)
+    {
+        return (int)name < functionLength - 1 ? name + 1 : 0;
+    }
+
+    public static FunctionName GetRandomFunctionNameOtherThan(FunctionName name)
+    {
+        var choice = (FunctionName)Random.Range(1, functionLength);
+        return choice == name ? 0 : choice;
+    }
+}
---
+++ Assets/Tutorial/Scripts/GPUGraph.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+
+public class GPUGraph : MonoBehaviour
+{
+    [SerializeField]
+    ComputeShader computeShader = default;
+
+    static readonly int 
+        positionsId = Shader.PropertyToID("_Positions"),
+        resolutionId = Shader.PropertyToID("_Resolution"),
+		stepId = Shader.PropertyToID("_Step"),
+		timeId = Shader.PropertyToID("_Time"),
+        modelToWorldID = Shader.PropertyToID("_ModelToWorld");
+
+    const int maxResolution = 400;
+
+    ComputeBuffer positionsBuffer;
+
+    [SerializeField]
+	Material material = default;
+
+	[SerializeField]
+	Mesh mesh = default;
+
+    [SerializeField, Range(10, maxResolution)]
+    int resolution = 10;
+
+    [SerializeField]
+    FunctionLibrary.FunctionName function = default;
+
+    public enum TransitionMode { Cycle, Random }
+
+    [SerializeField]
+    TransitionMode transitionMode = TransitionMode.Cycle;
+
+    [SerializeField, Min(0f)]
+    float functionDuration = 1f, transitionDuration = 1f;
+
+    float duration;
+
+    bool transitioning;
+
+    FunctionLibrary.FunctionName transitionFunction;
+
+    void OnEnable() 
+    {
+        positionsBuffer = new ComputeBuffer(maxResolution * maxResolution, 3 * 4);
+    }
+
+    void OnDisable()
+    {
+        positionsBuffer.Release();
+        positionsBuffer = null;
+    }
+
+    void Update()
+    {
+        duration += Time.deltaTime;
+        if (transitioning)
+        {
+            if (duration >= transitionDuration)
+            {
+                duration -= transitionDuration;
+                transitioning = false;
+            }
+        }
+        else if (duration >= functionDuration)
+        {
+            duration -= functionDuration;
+            transitioning = true;
+            transitionFunction = function;
+            PickNextFunction();
+        }
+
+        UpdateFunctionOnGPU();
+    }
+
+    void UpdateFunctionOnGPU () {
+		float step = 2f / resolution;
+		computeShader.SetInt(resolutionId, resolution);
+		computeShader.SetFloat(stepId, step);
+		computeShader.SetFloat(timeId, Time.time);
+		computeShader.SetMatrix(modelToWorldID, this.transform.localToWorldMatrix);
+
+        var kernelIndex = (int)function;
+        computeShader.SetBuffer(kernelIndex, positionsId, positionsBuffer);
+
+		int groups = Mathf.CeilToInt(resolution / 8f);
+        computeShader.Dispatch(kernelIndex, groups, groups, 1);
+
+        material.SetBuffer(positionsId, positionsBuffer);
+		material.SetFloat(stepId, step);
+        
+        var bounds = new Bounds(Vector3.zero, Vector3.one * (2f + 2f / resolution));
+		Graphics.DrawMeshInstancedProcedural(mesh, 0, material, bounds, resolution * resolution);
+	}
+
+    void PickNextFunction()
+    {
+        function = transitionMode == TransitionMode.Cycle ?
+            FunctionLibrary.GetNextFunctionName(function) :
+            FunctionLibrary.GetRandomFunctionNameOtherThan(function);
+    }
+}
