4cc4cd80db90b569e64d9964a04eaedb824cc830
vinceniko <vincent.nikolayev@gmail.com>
2021-04-23 03:18:08 -0400

    cleanup

--- Assets/Isosurface/Scripts/Common.hlsl
+++ Assets/Isosurface/Scripts/Common.hlsl
@@ -10,6 +10,8 @@ struct Edge {
     float3 end;
 };
 
+float opUnion( float d1, float d2 ) { return min(d1,d2); }
+
 float3 transformToCenter(float3 pos) {
     return (pos - _Resolution * 0.5) * _Step + _Step * 0.5;
 }
@@ -45,27 +47,64 @@ float3 linInterpolate(float3 p1, float3 p2, float d1, float d2) {
     return (1-d1/(d1-d2))*p1 + d1/(d1-d2)*p2;
 }
 
-void SetIsoVal(uint3 id, float val) 
+#define SHAPE_SIZE 0.5
+
+float Sphere(float3 p, float s)
 {
-	// id = mul(_ShapeToWorld, float4(id.zyx, 1.0)).xyz;
-	if (id.x < _Resolution && id.y < _Resolution && id.z < _Resolution) 
-    {
-		_IsoVals[threeDToOneD(id)] = val;
-	}
+	return length(p) - s;
 }
 
-void SetSurfacePoint(uint3 id, float3 val) 
+float Torus(float3 p, float s)
 {
-	// id = mul(_ShapeToWorld, float4(id.zyx, 1.0)).xyz;
-	if (id.x < _Resolution-1 && id.y < _Resolution-1 && id.z < _Resolution-1 && 
-    id.x >= 0 && id.y >= 0 && id.z >= 0 &&
-    val.x < _Resolution && val.y < _Resolution && val.z < _Resolution &&
-    val.x >= 0 && val.y >= 0 && val.z >= 0) 
-    {
-        val = transformToCenter(val);
-		_SurfacePoints[threeDToOneD(id)] = float4(val, 1.0);
-	} else {
-        val = transformToCenter(id);
-        _SurfacePoints[threeDToOneD(id)] = float4(val, 0.0);
-    }
+	float2 q = float2(length(p.xz)-s * 2.5,p.y);
+  	return length(q)-s;
 }
+
+float Pyramid(float3 p, float h)
+{
+  float m2 = h*h + 0.25;
+    
+  p.xz = abs(p.xz);
+  p.xz = (p.z>p.x) ? p.zx : p.xz;
+  p.xz -= 0.5;
+
+  float3 q = float3( p.z, h*p.y - 0.5*p.x, h*p.x + 0.5*p.y);
+   
+  float s = max(-q.x,0.0);
+  float t = clamp( (q.y-0.5*p.z)/(m2+0.25), 0.0, 1.0 );
+    
+  float a = m2*(q.x+s)*(q.x+s) + q.y*q.y;
+  float b = m2*(q.x+0.5*t)*(q.x+0.5*t) + (q.y-m2*t)*(q.y-m2*t);
+    
+  float d2 = min(q.y,-q.x*m2-q.y*0.5) > 0.0 ? 0.0 : min(a,b);
+    
+  return sqrt( (d2+q.z*q.z)/m2 ) * sign(max(q.z,-p.y));
+}
+
+float Octahedron(float3 p, float s)
+{
+  p = abs(p);
+  float m = p.x+p.y+p.z-s;
+  float3 q;
+       if( 3.0*p.x < m ) q = p.xyz;
+  else if( 3.0*p.y < m ) q = p.yzx;
+  else if( 3.0*p.z < m ) q = p.zxy;
+  else return m*0.57735027;
+    
+  float k = clamp(0.5*(q.z-q.y+s),0.0,s); 
+  return length(float3(q.x,q.y-s+k,q.z-k)); 
+}
+
+#define epsilon 0.0001
+#define ESTIMATE_NORMAL(function) \
+  float3 function##EstimateNormal(float3 p) { \
+    float x = function(float3(p.x+epsilon,p.y,p.z), SHAPE_SIZE) - function(float3(p.x-epsilon,p.y,p.z), SHAPE_SIZE); \
+    float y = function(float3(p.x,p.y+epsilon,p.z), SHAPE_SIZE) - function(float3(p.x,p.y-epsilon,p.z), SHAPE_SIZE); \
+    float z = function(float3(p.x,p.y,p.z+epsilon), SHAPE_SIZE) - function(float3(p.x,p.y,p.z-epsilon), SHAPE_SIZE); \
+    return normalize(float3(x,y,z)); \
+  }
+
+ESTIMATE_NORMAL(Sphere)
+ESTIMATE_NORMAL(Torus)
+ESTIMATE_NORMAL(Pyramid)
+ESTIMATE_NORMAL(Octahedron)
